ffb7fa4f89194cf03aceb3bf9daecfc9eb2ed471
Samuel Kleine <kle20001@byui.edu>
2023-01-25 13:47:27 -0700

    Added redis libraries, const, and connect at listen.

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 const bodyParser = require ('body-parser');
 
+const Redis = require ('redis');
+
+const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
+
 const {v4: uuidv4} = require('uuid');//universely unique identifier
 
 app.use(bodyParser.json()); //This looks for oncoming data
@@ -29,5 +33,6 @@ app.post('/login', (req, res) =>{
 });
 
 app.listen(port, () => {
+    redisClient.connect();
     console.log("listening");
 });
