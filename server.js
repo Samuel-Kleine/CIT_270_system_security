0931e1f50644bac9a555bcf70fa90adeb4e09779
Samuel Kleine <kle20001@byui.edu>
2023-02-27 13:43:17 -0700

    Added new cert and keys

--- server.js
+++ server.js
@@ -83,12 +83,12 @@ app.post('/login', async(req, res) =>{
 
 https.createServer(
     {
-    key: fs.readFileSync('./server.key'),
-    cert: fs.readFileSync('./server.cert'),
-    ca:fs.readFileSync('./chain.pem')
-},
-app
-).listen(port, ()=>{
-    redisClient.connect();
-    console.log('Listening on port: '+port);
+    key: fs.readFileSync('/etc/letsencrypt/live/samuelkleine.cit270.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/samuelkleine.cit270.com/cert.pem'),
+    ca:fs.readFileSync('/etc/letsencrypt/live/samuelkleine.cit270.com/fullchain.pem')
+    },
+    app
+    ).listen(port, ()=>{
+        redisClient.connect();
+        console.log('Listening on port: '+port);
 });
