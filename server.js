fb902d1eb0a14d18925fe3de92bd37661731b039
Samuel Kleine <kle20001@byui.edu>
2023-03-17 13:48:16 -0600

    Changed redis.yaml ports, changed redis client url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require ('body-parser');
 
 const Redis = require ('redis');
 
-const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
+const redisClient = Redis.createClient({url:"redis://default:nvnCxK1Zd9Yt5suuvCJ2@redis-stedi-samuel:6379"});
 
 const {v4: uuidv4} = require('uuid');//universely unique identifier
 
